a8e9f6ef14078fa32d439158a20b4162bc562194
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-30 16:21:10 +0530

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://74.201.240.43:8000/ChatBot/Sample_chatbot/hana_demo.xsjs'
-headers = {'Accept': 'application/json'}
-auth = ('SANYAM_K', 'Welcome@123')
-response = requests.get(url, headers=headers, auth=auth)
-
-with open('outputfile.json', 'w') as outf:
-    outf.write(response.content)
