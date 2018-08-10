c74e028783b354473937fb9b86e92d5e24fa6d64
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-10 16:05:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if($method == 'POST')
 			
 			
        		 }
-		$speech .="\r\n\n Which metro area will you prefer to get more info\n";
+	//	$speech .="\r\n\n Which metro area will you prefer to get more info\n";
 	}
 	else if ($com == 'gethousesal')
 	{
@@ -104,7 +104,7 @@ if($method == 'POST')
 			
 			
        		 }
-		$speech .="\r\n\n Which area will you prefer to get more info\n";
+		//$speech .="\r\n\n Which area will you prefer to get more info\n";
 	}
 	else if ($com == 'getcount')
 	{
